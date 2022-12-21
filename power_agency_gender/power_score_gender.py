204766fc8aba80151b08c5dc960af5b8e64d61c7
Sam <samuelkovaly05@gmail.com>
2022-12-20 21:52:14 -0500

    dominance full text analysis

--- power_agency_gender/power_score_gender.py
+++ power_agency_gender/power_score_gender.py
@@ -29,20 +29,41 @@ def agency_indicator(agency_string):
 
 def sum_of_actions(actions, verb_to_indicator, lemmatizer):
     indicator_sum = 0
+    count = 0
+    for action in actions:
+        verb = lemmatizer.lemmatize(action['w'])
+        if verb in verb_to_indicator and verb_to_indicator[verb] is not None:
+            indicator_sum += verb_to_indicator[verb]
+            count += 1
+        else:
+            pass # ignore for now
+    return indicator_sum, count
+
+
+def mean_of_actions(actions, verb_to_indicator, lemmatizer):
+    if len(actions) == 0:
+        return 0
+    indicator_sum = 0
     for action in actions:
         verb = lemmatizer.lemmatize(action['w'])
         if verb in verb_to_indicator and verb_to_indicator[verb] is not None:
             indicator_sum += verb_to_indicator[verb]
         else:
             pass # ignore for now
-    return indicator_sum
+    return indicator_sum / len(actions)
 
 
 def get_verb_to_score_dict(agency_power_frames_df, indicator_func, type):
     indicators = map(indicator_func, list(agency_power_frames_df[type]))
     return dict(zip(agency_power_frames_df['verb'], indicators))
 
-
+def get_dom_score(char, word_type, dom_to_score):
+    score = 0
+    for mod in char[word_type]:
+        mod_w = mod['w']
+        if mod_w in dom_to_score:
+            score += dom_to_score[mod_w]
+    return score
 
 def get_character_scores(translation):
 
@@ -54,10 +75,14 @@ def get_character_scores(translation):
 
     lemmatizer = WordNetLemmatizer()
 
+    dominance_df = pd.read_csv(f"./lexicon/NRC-VAD-Lexicon/BipolarScale/OneFilePerDimension/dominance-NRC-VAD-Lexicon.txt", sep='\t', names=['word', 'value'])
+    dom_to_score = dict(zip(dominance_df['word'], dominance_df['value']))
+
     characters = []
     genders = []
     power_scores = []
     agency_scores = []
+    dom_scores = []
 
     with open(book_file, 'r') as f:
         book_json = json.load(f)
@@ -68,22 +93,33 @@ def get_character_scores(translation):
                 genders.append(char['g']['argmax'])
 
                 # Power
-                agent_power = sum_of_actions(char['agent'], verb_to_power_score, lemmatizer)
-                patient_power = -1 * sum_of_actions(char['patient'], verb_to_power_score, lemmatizer)
-                power = agent_power + patient_power
-                power_scores.append(power)
+                agent_power, agent_count = sum_of_actions(char['agent'], verb_to_power_score, lemmatizer)
+                patient_power, patient_count = sum_of_actions(char['patient'], verb_to_power_score, lemmatizer)
+                power_count = agent_count + patient_count
+                if power_count > 0:
+                    power_scores.append(agent_power - patient_power)
+                else:
+                    power_scores.append(0)
 
                 # Agency
-                agency = sum_of_actions(char['agent'], verb_to_agency_score, lemmatizer)
-                agency_scores.append(agency)
-                
+                agency, agency_count = sum_of_actions(char['agent'], verb_to_agency_score, lemmatizer)
+                if agency_count > 0:
+                    agency_scores.append(agency)
+                else:
+                    agency_scores.append(0)
 
+                # Dominance
+                mod_score = get_dom_score(char, 'mod', dom_to_score)
+                poss_score = get_dom_score(char, 'poss', dom_to_score)
+                dom_scores.append(mod_score + poss_score)
+                
 
     char_scores = pd.DataFrame()
     char_scores['character'] = characters
     char_scores['gender'] = genders
     char_scores['power_score'] = power_scores
     char_scores['agency_score'] = agency_scores
+    char_scores['dom_score'] = dom_scores
     return char_scores
 
     #'characters': ['agent', 'patient', 'mod', 'poss', 'id', 'g', 'count', 'mentions']
@@ -91,7 +127,7 @@ def get_character_scores(translation):
 
 
 def get_gender_statistics(char_scores):
-    gender_sum = char_scores.groupby('gender').agg(power_sum=('power_score', 'sum'), agency_sum=('agency_score', 'sum'), count=('character', 'nunique'), power_mean=('power_score', 'mean'), agency_mean=('agency_score', 'mean'))
+    gender_sum = char_scores.groupby('gender').agg(power_sum=('power_score', 'sum'), agency_sum=('agency_score', 'sum'), dominance_sum=('dom_score', 'sum'), count=('character', 'nunique'), power_mean=('power_score', 'mean'), agency_mean=('agency_score', 'mean'), dom_mean=('dom_score', 'mean'))
     return gender_sum
 
 def get_top_power_agency(char_scores):
@@ -115,6 +151,7 @@ def main():
         top_power, top_agency = get_top_power_agency(char_scores)
         top_power.to_csv(f"./power_agency_gender/{translation}/top_power.csv")
         top_agency.to_csv(f"./power_agency_gender/{translation}/top_agency.csv")
+        print(f"Successfully saved {translation} .csv files")
 
 if __name__ == "__main__":
     main()
